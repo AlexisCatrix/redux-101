31e287a9eac84026900cf04994a2d7bf1effa9c0
Loic <loic.brassart@gmail.com>
2021-01-12 10:09:45 +0100

    Add code to write in the store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function App() {
   const isLoading = useSelector(state => state.isLoading);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <p>Status: {isLoading ? "Yep!" : "Nope"}</p>
+      <button onClick={() => {
+        dispatch({ type: "LOADER_ENABLE" })
+      }}>Toggle loader</button>
     </div>
   );
 }
